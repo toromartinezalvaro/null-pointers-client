e23e63cbe5ec9fa0c5600814a255069498ae557f
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-19 20:42:45 -0500

    Adding preferences

---
+++ null-pointers-client/app/routes/reports.preferences.tsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "@remix-run/react";
+import styles from "~/styles/preferences.css";
+import { preferencesLoader } from "../loaders/preferencesLoader";
+import { Preferencia } from "../interfaces/preference";
+import PreferencesTable from "../components/PreferencesTable";
+
+export const loader = preferencesLoader;
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+export default function Preferences() {
+  const preferenciasData = useLoaderData<Preferencia[]>();
+
+  if (!Array.isArray(preferenciasData) || preferenciasData.length === 0) {
+    return <p className="no-data">No hay datos de preferencias disponibles.</p>;
+  }
+
+  return (
+    <div className="container">
+      <PreferencesTable preferenciasData={preferenciasData} />
+    </div>
+  );
+}

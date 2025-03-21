3167cbf087d78ef829fd245efcd466133a5f8f84
YasminOspina <sirleyob244@gmail.com>
2025-03-21 18:45:39 -0500

    Under construction, I modify the loader, root and image paths.

--- null-pointers-client/app/root.tsx
+++ null-pointers-client/app/root.tsx
@@ -21,7 +21,8 @@ export const links: LinksFunction = () => [
     rel: "preconnect", href: "https://fonts.googleapis.com"
 },
 {
-    href: "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap", rel: "stylesheet"}
+    href: "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap", rel: "stylesheet"},
+    { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" },
 ];
 
 

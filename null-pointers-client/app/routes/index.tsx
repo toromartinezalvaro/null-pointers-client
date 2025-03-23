0823c872f07f7cd752580e7d985c2abe36940ad4
aura <angelatorres97@hotmail.com>
2025-03-23 09:46:31 -0500

    se agrega inicio a routes cono la ruta index

---
+++ null-pointers-client/app/routes/index.tsx
@@ -0,0 +1,17 @@
+import Start from "~/components/Start";
+import "~/styles/start.css";
+
+
+export default function Index() {
+
+const imageUrl = "/assets/img/image1.png";
+
+  return (
+    <div className="start">
+      
+       <Start imageSrc={imageUrl} />
+    </div>
+
+
+  );
+}

f1cf41c6b62185d112e534d714f616f4b87ccdb0
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-25 00:41:08 -0500

    modifying index styles

--- null-pointers-client/app/routes/index.tsx
+++ null-pointers-client/app/routes/index.tsx
@@ -1,16 +1,12 @@
 import Start from "~/components/Start";
 import "~/styles/start.css";
 
-
 export default function Index() {
-
-const imageUrl = "/assets/img/image1.png";
+  const imageUrl = "/assets/img/image1.png";
 
   return (
     <div className="start">
-       <Start imageSrc={imageUrl} />
+      <Start imageSrc={imageUrl} />
     </div>
-
-
   );
 }

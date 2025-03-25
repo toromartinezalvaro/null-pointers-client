05a16d9e229a2eb209c774c4a615e656f25a29b2
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-24 20:49:37 -0500

    Solving error on authenticator

--- null-pointers-client/app/routes/reports.preferences.tsx
+++ null-pointers-client/app/routes/reports.preferences.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Navigate } from "@remix-run/react";
 import "~/styles/preferences.css";
 import { preferencesLoader } from "../loaders/preferencesLoader";
 import { Preferencia } from "../interfaces/preference";
@@ -6,22 +6,21 @@ import PreferencesTable from "../components/PreferencesTable";
 
 /*logica para proteger vistas*/
 import { useAuth } from "~/hooks/useAuth";
-import { Navigate } from "@remix-run/react";
 
 export const loader = preferencesLoader;
 
 export default function Preferences() {
   const preferenciasData = useLoaderData<Preferencia[]>();
 
+  /*logica para proteger vistas*/
+  const { authorized } = useAuth("Administrador");
+  if (!authorized) {
+    return <Navigate to="/" replace />;
+  }
+
   if (!Array.isArray(preferenciasData) || preferenciasData.length === 0) {
     return <p className="no-data">No hay datos de preferencias disponibles.</p>;
   }
-
-    /*logica para proteger vistas*/
-    const { authorized, reason } = useAuth("Administrador");
-    if (!authorized) {
-      return <Navigate to="/" replace />;
-    }
 
   return (
     <div className="container">

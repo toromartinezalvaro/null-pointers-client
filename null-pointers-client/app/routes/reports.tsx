23bb4620a18b2768805e6f19a45a033dae6919a6
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-24 20:51:25 -0500

    Adding destinations

--- null-pointers-client/app/routes/reports.tsx
+++ null-pointers-client/app/routes/reports.tsx
@@ -1,11 +1,10 @@
-import { Outlet } from "@remix-run/react";
+import { Outlet, Navigate } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import reportsStylesHref from "~/styles/reports.css?url";
 import ReportsAsideMenu from "~/components/ReportsAsideMenu";
 
 /*logica para proteger vistas*/
 import { useAuth } from "~/hooks/useAuth";
-import { Navigate } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: reportsStylesHref },
@@ -14,7 +13,7 @@ export const links: LinksFunction = () => [
 export default function ReportsLayout() {
 
   /*logica para proteger vistas*/
-  const { authorized, reason } = useAuth("Administrador");
+  const { authorized } = useAuth("Administrador");
 
   if (!authorized) {
     return <Navigate to="/login" replace />;

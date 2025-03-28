15e530add0fd504243291d68a26bc5897f48d8cb
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-27 21:05:20 -0500

    Adding new styles to register form

--- null-pointers-client/app/routes/record.tsx
+++ null-pointers-client/app/routes/record.tsx
@@ -2,11 +2,9 @@ import RegisterForm from "~/components/RegisterForm";
 import "~/styles/record.css";
 
 export default function RecordPage() {
-  const imageUrl = "/assets/img/registro.png";
 
   return (
     <div className="record-container">
-      <img className="imgrec" src={imageUrl} alt="Register"/>
       <RegisterForm />
     </div>
   );

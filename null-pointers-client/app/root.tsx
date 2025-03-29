28e229bc196fa59f09f72fddd4a3fe8e1016217e
toro-martinez-alvaro <torom.alvaro@gmail.com>
2025-03-28 19:17:58 -0500

    Adding additional configuration

--- null-pointers-client/app/root.tsx
+++ null-pointers-client/app/root.tsx
@@ -14,6 +14,7 @@ export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap",
+    defer: true,
   },
   {
     rel: "preconnect",
@@ -22,12 +23,13 @@ export const links: LinksFunction = () => [
   {
     href: "https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
     rel: "stylesheet",
+    defer: true,
   },
 ];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -35,13 +37,17 @@ export default function App() {
         <Links />
       </head>
       <body>
-          <MenuNavegacion />
-          <div id="detail">
-            <Outlet />
-          </div>
-          <ScrollRestoration />
-          <Scripts />
+        <MenuNavegacion />
+        <div id="detail">
+          <Outlet />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <noscript>
+          Debes habilitar JavaScript para usar esta aplicación.
+        </noscript>
       </body>
     </html>
   );
 }
+
